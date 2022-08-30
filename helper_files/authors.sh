d3616b8bf26e4b48e1bdddd0f79d8f369f97595c
ngugimuchangi <tech.curious14@gmail.com>
2022-08-30 18:07:04 +0000

    update: authors files and script

--- helper_files/authors.sh
+++ helper_files/authors.sh
@@ -3,7 +3,7 @@ set -e
 {
 	cat <<- 'EOH'
 		# This file lists all individuals having contributed content to the repository.
-		# For how it is generated, see `hack/generate-authors.sh`.
+		# For how it is generated, see `helper_files/authors.sh`.
 	EOH
 	echo
 	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
