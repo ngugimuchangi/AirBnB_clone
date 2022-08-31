1e9cddcb0ffc1022604ed6286a16ebb2248584fd
ngugimuchangi <tech.curious14@gmail.com>
2022-08-31 07:52:48 +0000

    fix: check for nested quotation marks

--- console.py
+++ console.py
@@ -2,15 +2,28 @@
 """ Command line interpreter module
 """
 import cmd
-from models.base_model import BaseModel, storage
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """ Command line interpreter class
+        Private attributes:
+            __classes: list of classes
+        Public class attributes:
+            cmd.Comd.prompt: prompt to display for interactive mode
+        Public instance methods:
+            Command methods:
+                do_create, do_show, do_destroy, do_upate, do_all, do_EOF,
+                do_quit
+            Help methods:
+                help_create, help_show, help_destroy,help_update,
+                help_all, help_EOF, help_quit, help_help
     """
 
     cmd.Cmd.prompt = '(hbnb) '
-    __classes = ["BaseModel"]
+    __classes = ["BaseModel", "User", "State", "City",
+                 "Amenity", "Place", "Review"]
 
     def do_create(self, line):
         """ Creates new BaseModel instance,
@@ -20,8 +33,8 @@ class HBNBCommand(cmd.Cmd):
             Return: nothing
         """
         if line:
-            args = line.split()
-            if args[0] in HBNBCommand.__classes:
+            class_name = line.split()[0].strip('"')
+            if class_name in HBNBCommand.__classes:
                 new_object = BaseModel()
                 new_object.save()
                 print(new_object.id)
@@ -38,8 +51,9 @@ class HBNBCommand(cmd.Cmd):
             Return: nothing
         """
         if HBNBCommand.validate(line):
+            obj_id = line.split()[1].strip('"')
             for key in storage._FileStorage__objects.keys():
-                if line.split()[1] == storage._FileStorage__objects[key].id:
+                if storage._FileStorage__objects[key].id == obj_id:
                     print(storage._FileStorage__objects[key])
                     break
 
@@ -50,8 +64,9 @@ class HBNBCommand(cmd.Cmd):
             Return: nothing
         """
         if HBNBCommand.validate(line):
+            obj_id = line.split()[1].strip('"')
             for key in storage._FileStorage__objects.keys():
-                if line.split()[1] == storage._FileStorage__objects[key].id:
+                if storage._FileStorage__objects[key].id == obj_id:
                     del(storage._FileStorage__objects[key])
                     storage.save()
                     break
@@ -64,11 +79,12 @@ class HBNBCommand(cmd.Cmd):
             Return: nothing
         """
         if line:
-            if line in HBNBCommand.__classes:
+            class_name = line.split()[0].strip('"')
+            if class_name in HBNBCommand.__classes:
                 my_objects = []
                 for i in storage._FileStorage__objects.keys():
                     if storage._FileStorage__objects[i].__class__.__name__ == \
-                       line.split()[0]:
+                       class_name:
                         my_objects.append(str(
                             storage._FileStorage__objects[i]))
             else:
@@ -78,6 +94,24 @@ class HBNBCommand(cmd.Cmd):
             my_objects = [str(storage._FileStorage__objects[i])
                           for i in storage._FileStorage__objects.keys()]
         print(my_objects)
+
+    def do_update(self, line):
+        """ Update an object's existing attribute or add new
+            attribute to the object
+            Args:
+                line (str): class name, object's id, attribute name,
+                            attribute value
+            Return: nothing
+        """
+        if HBNBCommand.validate(line) and \
+           HBNBCommand.validate_attr(line.split()):
+            class_name, obj_id, attr_name, attr_val = line.split()[:4]
+            for i in storage._FileStorage__objects.keys():
+                if storage._FileStorage__objects[i].id == obj_id.strip('"'):
+                    obj = storage._FileStorage__objects[i]
+                    setattr(obj, attr_name.strip('"'), attr_val.strip('"'))
+                    obj.save()
+                    break
 
     def do_quit(self, line):
         """ Exit program gracefully
@@ -91,6 +125,7 @@ class HBNBCommand(cmd.Cmd):
             Args: none
             Return: True
         """
+        print()
         return True
 
     def emptyline(self):
@@ -122,6 +157,13 @@ class HBNBCommand(cmd.Cmd):
               "Display all objects or all objects of a specific",
               "class\n")
 
+    def help_update(self):
+        """ Help function for update command """
+        print("Usage: update <class name> <id> <attribute name>",
+              '"<attribute value>"\n' + "Update command to search for",
+              "object by id and update it using passed",
+              "attribute name and value\n")
+
     def help_quit(self):
         """ Help function for quit command """
         print("Quit command to exit the program gracefully\n")
@@ -132,7 +174,7 @@ class HBNBCommand(cmd.Cmd):
 
     def help_help(self):
         """ Help function for help command """
-        print("Usage: help <topic/command>\nHelp command to display ",
+        print("Usage: help [topic/command]\nHelp command to display",
               "information about a commands\n")
 
     @staticmethod
@@ -173,13 +215,15 @@ class HBNBCommand(cmd.Cmd):
             Args:
                 line (str): user input
             Return: True if input passes all validation
-                    Falase if input fails one of the checks
+                    False if input fails one of the checks
         """
         if line:
             args = line.split()
-            if args[0] in HBNBCommand.__classes:
+            class_name = args[0].strip('"')
+            if class_name in HBNBCommand.__classes:
                 if len(args) > 1:
-                    if any(storage._FileStorage__objects[i].id == args[1]
+                    obj_id = args[1].strip('"')
+                    if any(storage._FileStorage__objects[i].id == obj_id
                            for i in storage._FileStorage__objects.keys()):
                         return True
                     else:
@@ -190,6 +234,22 @@ class HBNBCommand(cmd.Cmd):
                 HBNBCommand.class_exists()
         else:
             HBNBCommand.class_missing()
+        return False
+
+    @staticmethod
+    def validate_attr(args):
+        """ Checks if attribute name and value are passed
+            in do_upate method
+            Args:
+                args: list of arguments
+            Return: nothing
+        """
+        if len(args) < 3:
+            print("** attribute name missing **")
+        elif len(args) < 4:
+            print("** value missing **")
+        else:
+            return True
         return False
 
 
